f911c2f63a14cd32dac981594b670f0e92e84c4f
diana-aires <aires1986@hotmail.com>
2026-06-07 17:52:47 -0300

    Update LancamentoForm.jsx

--- src/components/Lancamentos/LancamentoForm.jsx
+++ src/components/Lancamentos/LancamentoForm.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { supabase } from '../../lib/supabase'; // Ajuste o caminho conforme sua configuração
+import { sb } from '../../services/supabase';
 import { C, styles } from '../../styles/theme';
 import { CARTOES } from '../../utils/constants';
 
